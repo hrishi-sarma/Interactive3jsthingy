1ce8c8e313e14ee2064060f90c6378cd15c1e99d
hrishi-sarma <sarmahrishi05@gmail.com>
2024-07-19 18:17:19 +0530

    updates

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'jsm/loaders/GLTFLoader.js';
 
-// Scene
 const scene = new THREE.Scene();
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 5.5, 18);
-camera.rotation.set(0,0,0);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -16,20 +14,41 @@ renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 // Lighting
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(5, 10, 7.5);
+const directionalLight = new THREE.PointLight(0xffffff, 500);
+directionalLight.position.set(-30, 40, 1);
 scene.add(directionalLight);
 
 // GLTF Loader
-let car;
+let mesh, building;
 const loader = new GLTFLoader();
 loader.load('3dModels/stairs.glb', (gltf) => {
-    car = gltf.scene;
-    scene.add(car);
-    car.position.set(0, 0, 0);
+    mesh = gltf.scene;
+
+    mesh.traverse((child) => {
+        if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    });
+
+    scene.add(mesh);
+    mesh.position.set(0, 0, 0);
+}, undefined, (error) => {
+    console.error(error);
+});
+
+loader.load('3dModels/building.glb', (gltf2) => {
+    building = gltf2.scene;
+
+    building.traverse((child) => {
+        if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    });
+
+    scene.add(building);
+    building.position.set(0, 0, 0);
 }, undefined, (error) => {
     console.error(error);
 });
@@ -41,26 +60,24 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-
 // Animation loop
 const animate = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 };
 
-const carSpeed = 0.5;
+const meshSpeed = 1;
 const minYPosition = 5.5; // Minimum Y position for the camera
 const maxYPosition = 42;
+
 function moveCameraOnScroll() {
     window.addEventListener('wheel', function(event) {
-        if (car) {
-            if (event.deltaY > 0) {
-                camera.position.y = Math.max(minYPosition, camera.position.y - carSpeed);
-                 // Scroll down
-            } else {
-                camera.position.y = Math.min(maxYPosition, camera.position.y + carSpeed);
-                 // Scroll up
-            }
+        if (event.deltaY > 0) {
+            // Scroll down
+            camera.position.y = Math.max(minYPosition, camera.position.y - meshSpeed);
+        } else {
+            // Scroll up
+            camera.position.y = Math.min(maxYPosition, camera.position.y + meshSpeed);
         }
     });
 }
