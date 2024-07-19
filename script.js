bd02f916b2cd4eeb850b973caa146c2bd3cea805
hrishi-sarma <sarmahrishi05@gmail.com>
2024-07-19 08:56:35 +0530

    new changes

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ import { GLTFLoader } from 'jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 2, 5);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 5.5, 18);
+camera.rotation.set(0,0,0);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -25,7 +26,7 @@ scene.add(directionalLight);
 // GLTF Loader
 let car;
 const loader = new GLTFLoader();
-loader.load('3dModels/car1.glb', (gltf) => {
+loader.load('3dModels/stairs.glb', (gltf) => {
     car = gltf.scene;
     scene.add(car);
     car.position.set(0, 0, 0);
@@ -48,30 +49,21 @@ const animate = () => {
 };
 
 const carSpeed = 0.5;
-function moveCar() {
-    document.addEventListener('keydown', function(event) {
+const minYPosition = 5.5; // Minimum Y position for the camera
+const maxYPosition = 42;
+function moveCameraOnScroll() {
+    window.addEventListener('wheel', function(event) {
         if (car) {
-            switch(event.key) {
-                case 'w':
-                    
-                    camera.position.z -= carSpeed;
-                    break;
-                case 's':
-                   
-                    camera.position.z += carSpeed;
-                    break;
-                case 'a':
-                    
-                    camera.position.x -= carSpeed;
-                    break;
-                case 'd':
-                   
-                    camera.position.x += carSpeed;
-                    break;
+            if (event.deltaY > 0) {
+                camera.position.y = Math.max(minYPosition, camera.position.y - carSpeed);
+                 // Scroll down
+            } else {
+                camera.position.y = Math.min(maxYPosition, camera.position.y + carSpeed);
+                 // Scroll up
             }
         }
     });
 }
 
-moveCar();
+moveCameraOnScroll();
 animate();
